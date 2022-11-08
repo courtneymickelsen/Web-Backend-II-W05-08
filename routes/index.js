fecb345e2b8aff39687f10a513127c762ef5bad1
courtneymickelsen <lam15020@byui.edu>
2022-11-07 23:15:57 -0700

    just finishing up with a few bugs

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const customersRoutes = require('./customers');
 const ordersRoutes = require('./orders');
 const authRoutes = require('./auth');
 const { home } = require('../controllers/home');
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
 routes.get('/', home);
 routes.get('/customers', customersRoutes);
